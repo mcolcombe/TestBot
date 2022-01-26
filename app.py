e77f216e6f9a1248076d38226875bb7022357c6d
Idan Mika <idanmi25@gmail.com>
2022-01-26 00:34:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def msg_received_from_group():
     send_msg("Time to sleep on this side of the bed!")
 	
 
-  elif data['text'].lower() == "!testpic":
-    send_msg_pic("Hello World!","https://i.groupme.com/1024x1024.jpeg.d733d6de5c36462f8d1cb67e3191b618")
+  #elif data['text'].lower() == "!testpic":
+  #  send_msg_pic("Hello World!","https://i.groupme.com/1024x1024.jpeg.d733d6de5c36462f8d1cb67e3191b618")
 	
 	
 
