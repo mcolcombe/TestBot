a9972a9ea07a090c5705aa77aa4c60df3d4f35e9
mcolcombe <98493450+mcolcombe@users.noreply.github.com>
2022-01-26 17:55:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def msg_received_from_group():
   
   #Check the text of the message sent to the chat to see if it matches our command word
   if data['text'].lower() == "!test":
-    send_msg("Hello")
+    send_msg("Hello I'm Megan")
 	
 
   return "ok", 200
@@ -28,7 +28,7 @@ def msg_received_from_group():
 #Sends a message to the chat that the bot originates from
 def send_msg(msg):
   url  = 'https://api.groupme.com/v3/bots/post'
-  payload = { 'text' : msg, 'bot_id' : 'e4674e778e9972ea3137611978'}
+  payload = { 'text' : msg, 'bot_id' : '6c2b350e4f92a451d063e670cf'}
   r = requests.post(url, data=json.dumps(payload))
 
 #logging function to help debug
